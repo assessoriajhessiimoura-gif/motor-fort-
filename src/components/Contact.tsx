379e85cf87cc8d2e77f96de12708dabc6a7a66e6
assessoriajhessiimoura-gif <assessoriajhessiimoura@gmail.com>
2025-09-12 23:16:14 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -134,7 +134,7 @@ Mensagem: ${formData.message}`;
                     href="mailto:assessoriamotorfort@gmail.com"
                     className="text-red-600 hover:text-red-700 font-medium"
                   >
-                    contato@motorfort.com.br
+                   @motor.fortt
                   </a>
                 </div>
               </div>
